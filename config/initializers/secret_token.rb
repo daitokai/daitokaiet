59845a0ada16ad8723066497f81d4f2d46909ce0
LuckOfWise <luck.of.wise@gmail.com>
2013-12-18 05:52:59 +0900

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Daitokaiet::Application.config.secret_key_base = '2af10e12ec381ea903abc512a02009ca47d262a9c2cafdd8770f746bc7955ab534081d44a3a50a487b6429c444f998f25ef79923cb484975402de147622f21d7'
