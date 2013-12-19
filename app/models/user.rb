ec3c2984bf35b80d9301d6ab2752bf1c1f9300a9
LuckOfWise <luck.of.wise@gmail.com>
2013-12-19 11:49:42 +0900

    Fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   has_many :records, dependent: :destroy
   validates :goal, numericality: true
 
-  scope :social_visible, -> { joins(:records) }
+  scope :social_visible, -> { joins(:records).uniq }
 
   def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
     user = User.where(provider: auth.provider, uid: auth.uid).first
