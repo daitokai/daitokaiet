47f33492d3db60b51327a4f65190cc596b806c0e
LuckOfWise <luck.of.wise@gmail.com>
2013-12-20 17:22:41 +0900

    followにユニーク指定追加

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,6 @@
 class Follow < ActiveRecord::Base
   belongs_to :user
   belongs_to :target_user, class_name: 'User'
+
+  validates :target_user, presence: true, uniqueness: {scope: [:user_id]}
 end
