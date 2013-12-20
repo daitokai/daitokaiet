fa15678325eacf47f2241913f77a4ef38eeabf23
LuckOfWise <luck.of.wise@gmail.com>
2013-12-20 17:15:25 +0900

    不要なメソッドを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,9 @@ class User < ActiveRecord::Base
 
   validates :goal, numericality: true
 
-  scope :social_visible, -> { joins(:records).uniq }
-
   def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
     user = User.where(provider: auth.provider, uid: auth.uid).first
     data = auth['info']
-    logger.debug(data)
     unless user
       user = User.create(name: auth.info.nickname,
                          provider: auth.provider,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,23 +2,5 @@
 require 'spec_helper'
 
 describe User do
-  describe '.social_visible' do
-    let!(:user) { User.create!(
-        goal: 60,
-        email: User.create_unique_email,
-        password: 'x'*8
-    )}
-
-    it '記録がない' do
-      expect(User.count).to eq(1)
-      expect(User.social_visible.count).to eq(0)
-    end
-
-    it '記録がある' do
-      record = user.records.build(target_date: Date.today)
-      record.stub(:update_twitter)
-      record.save!
-      expect(User.social_visible.count).to eq(1)
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
