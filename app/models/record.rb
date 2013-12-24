d33313c15103e0e2fc1100d3a8b958d4bdcceee9
LuckOfWise <luck.of.wise@gmail.com>
2013-12-24 11:47:23 +0900

    リファクタリング

--- app/models/record.rb
+++ app/models/record.rb
@@ -17,28 +17,27 @@ class Record < ActiveRecord::Base
   end
 
   def previous
-    date = self.target_date
-    self.user
-      .records
-      .where{ target_date < date }
-      .order(target_date: :desc)
-      .first
+    previous_or_next(:previous)
   end
 
-  def get_next
-    date = self.target_date
-    self.user
-      .records
-      .where{ target_date > date }
-      .order(target_date: :asc)
-      .first
+  def next_record
+    previous_or_next(:next)
   end
 
-  def calc_supplement_weight (date, user)
-    self.target_date = date
-    self.user = user
+  def previous_or_next(type)
+    date = self.target_date
+    records = self.user.records
+    if type == :previous
+      records = records.where { target_date < date }.order(target_date: :desc)
+    else
+      records = records.where { target_date > date }.order(target_date: :asc)
+    end
+    records.first
+  end
+
+  def calc_supplement_weight
     if prev_data = self.previous
-      if next_data = self.get_next
+      if next_data = self.next_record
         weight_delta = next_data.weight.to_f - prev_data.weight.to_f
         date_delta = next_data.target_date - prev_data.target_date
         self.weight = prev_data.weight.to_f + ((weight_delta.to_f / date_delta) * (self.target_date - prev_data.target_date))
