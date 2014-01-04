8fa44cdc2fc0e93d1f113f5a2aa4d261ef3227a8
LuckOfWise <luck.of.wise@gmail.com>
2014-01-05 05:56:03 +0900

    jquery-mobileの画像がproduction環境で出ない問題に対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Daitokaiet::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( application_mobile.js application_mobile.css )
+  config.assets.precompile += %w( application_mobile.js application_mobile.css *.gif *.png *.jpg )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
